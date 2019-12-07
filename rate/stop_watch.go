735f972fafebd35c846d2f640f9e9a2b34c6bb66
adam <adam.wu@shein.com>
2019-12-07 18:09:56 +0800

    update stop watch

--- rate/stop_watch.go
+++ rate/stop_watch.go
@@ -9,17 +9,35 @@ type Watch struct {
 }
 
 func (w *Watch) start() {
+	if w.running {
+		return
+	}
 	w.running = true
 	w.startTick = time.Now().UnixNano()
 }
 
 func (w *Watch) stop() {
+	if !w.running {
+		return
+	}
 	w.running = false
 	w.elapsedNanos += time.Now().UnixNano() - w.startTick
 }
 
 func (w *Watch) reset() {
-
+	if !w.running {
+		return
+	}
+	w.running = false
+	w.elapsedNanos += time.Now().UnixNano() - w.startTick
+	w.startTick = 0
+}
+func (w Watch) elapse() int64 {
+	if w.running {
+		return w.elapsedNanos + time.Now().UnixNano() - w.startTick
+	} else {
+		return w.elapsedNanos
+	}
 }
 
 func (w Watch) isRunning() bool {
