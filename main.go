95588062a1796de7a0a26249d173cc70bb2c2eb5
adam <adam.wu@shein.com>
2019-12-19 13:42:25 +0800

    fix time bug and add error handle

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"goslow/rate"
+	"math"
 	"net/http"
 	"strings"
 	"time"
@@ -23,7 +24,30 @@ func (m myHandle) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	_, _ = writer.Write([]byte("I have been waiting :" + result.String()))
 }
 func main() {
-	http.Handle("/acquire", myHandle{})
-	err := http.ListenAndServe("localhost:80", nil)
-	fmt.Println(err)
+	//http.Handle("/acquire", myHandle{})
+	//err := http.ListenAndServe("localhost:8080", nil)
+	//fmt.Println(err)
+	sum := 0.0
+	account := 0.0
+	var rater float64
+	for i := 0; i < 36; i++ {
+		if sum > 10000 {
+			rater = 0.024
+		} else {
+			rater = 0.04
+		}
+		sum = sum * math.Pow(rater/365+1, 30)
+		if sum > 10000 {
+			account += 710
+			if account > 1000 {
+				sum += 1000
+				account = account - 1000
+			}
+		} else {
+			sum += 710
+		}
+
+		fmt.Printf("第%d个月收益为 %f元\n", i+1, sum)
+	}
+	fmt.Printf("还剩%f 元未加入计算利息", account)
 }
