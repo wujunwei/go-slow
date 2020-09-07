f745479e0e2b62df206063c872a57ab446f899c0
adam <wjw3323@live.com>
2020-09-07 11:38:32 +0800

    change test case

--- main.go
+++ main.go
@@ -24,15 +24,7 @@ func (m myHandle) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	_, _ = writer.Write([]byte("I have been waiting :" + result.String()))
 }
 func main() {
-	//http.Handle("/", myHandle{})
-	//err := http.ListenAndServe("localhost:8080", nil)
-	//fmt.Println(err)
-	for i := 0; i < 10; i++ {
-		go func() {
-			t, _ := l.Acquire()
-			fmt.Println("I have been waiting :" + t.String())
-		}()
-
-	}
-	time.Sleep(7 * time.Second)
+	http.Handle("/", myHandle{})
+	err := http.ListenAndServe("localhost:8080", nil)
+	fmt.Println(err)
 }
